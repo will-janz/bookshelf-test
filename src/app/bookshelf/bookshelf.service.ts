4c0e7a6dff131b9353b49a129d8a686dbfb72e7c
Will J <willj.94@gmail.com>
2018-03-22 00:32:29 -0600

    feat: add quick bookshelf service

--- src/app/bookshelf/bookshelf.service.ts
+++ src/app/bookshelf/bookshelf.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class BookshelfService {
+  readonly API_URL = 'https://www.googleapis.com/books/v1/volumes';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getBooks(q) {
+    // Should be an array of books but "any" will do for now
+    // Should use HttpParams but this is quicker
+    return this.http.get<any[]>(`${this.API_URL}?q=${q}`);
+  }
 }
