ac438277706f75d8781e3fd90aedb8b5e13b3c13
Will J <willj.94@gmail.com>
2018-03-22 11:09:29 -0600

    feat: add ability to search for books and display in a nice table
    Added several angular material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule,
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -17,7 +25,12 @@ import { BookshelfService } from './bookshelf/bookshelf.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
   ],
   providers: [
     BookshelfService,
