9b07ccf0b75a219ef86019d842496aba7190b320
Will J <willj.94@gmail.com>
2018-03-22 11:09:29 -0600

    feat: add ability to search for books and display in a nice table
    Added several angular material dependencies

--- src/app/bookshelf/bookshelf.component.ts
+++ src/app/bookshelf/bookshelf.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { BookshelfService } from './bookshelf.service';
-import { Bookshelf } from './bookshelf';
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { Subject, Observable } from 'rxjs/Rx';
 
+import { BookshelfService } from './bookshelf.service';
 
 @Component({
   selector: 'bt-bookshelf',
   templateUrl: './bookshelf.component.html',
   styleUrls: ['./bookshelf.component.css'],
 })
-export class BookshelfComponent implements OnInit {
+export class BookshelfComponent {
+  // Table definitions
+  displayedColumns = ['title', 'actions'];
+  dataSource = new MatTableDataSource();
 
-  books: Array<any> = [{ items: [] }];
+  // Search is subject so we can debounce it and not overload the API
+  public searchBooks = new Subject<string>();
 
-  constructor(private service: BookshelfService) { }
-
-  ngOnInit() {
-    this.service.getBooks('harry').subscribe(books => {
-      this.books = books;
-    });
+  constructor(private service: BookshelfService) {
+    // Subscribe to search keyup events, debounce, then query Google
+    const subscription = this.searchBooks
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .flatMap(search => Observable.of(search))
+      .subscribe(searchTerm => {
+        // Perform the search
+        this.service.getBooks('harry').subscribe(books => {
+          this.dataSource.data = books.items;
+        });
+      });
   }
-
 }
