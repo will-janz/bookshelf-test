0cb85af6047efa70f9742e0c8974cb36788111d1
Will J <willj.94@gmail.com>
2018-03-28 23:20:06 -0600

    test: bring unit tests back up to speed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,14 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSortModule,
+  MatTableModule,
+} from '@angular/material';
 
 import { BookshelfComponent } from './bookshelf/bookshelf.component';
 import { BookshelfService } from './bookshelf/bookshelf.service';
@@ -11,6 +19,12 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSortModule,
+        MatTableModule,
       ],
       declarations: [
         AppComponent,
--- src/app/bookshelf/bookshelf.component.spec.ts
+++ src/app/bookshelf/bookshelf.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSortModule,
+  MatTableModule,
+} from '@angular/material';
 
 import { BookshelfComponent } from './bookshelf.component';
 import { BookshelfService } from './bookshelf.service';
@@ -12,7 +21,14 @@ describe('BookshelfComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        BrowserAnimationsModule
         HttpClientModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSortModule,
+        MatTableModule,
       ],
       declarations: [
         BookshelfComponent,
